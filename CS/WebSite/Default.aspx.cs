e74862722e8543b7fbd9ba618f4e04418782ca5c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Init(object sender, EventArgs e) {
+    }
+    protected void ASPxGridView1_CustomCallback(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewCustomCallbackEventArgs e) {
+        ASPxGridView grid = sender as ASPxGridView;
+        ChangePageIndex(grid, e.Parameters);
+    }
+    private void ChangePageIndex(ASPxGridView grid, string parameters) {
+        if(Convert.ToInt32(parameters) < 0) {
+            if(grid.PageIndex < grid.PageCount - 1)
+                grid.PageIndex++;
+            else
+                grid.PageIndex = 0;
+        }
+        else {
+            if(grid.PageIndex > 0)
+                grid.PageIndex--;
+            else
+                grid.PageIndex = grid.PageCount;
+        }
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+	End Sub
+	Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewCustomCallbackEventArgs)
+		Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+		ChangePageIndex(grid, e.Parameters)
+	End Sub
+	Private Sub ChangePageIndex(ByVal grid As ASPxGridView, ByVal parameters As String)
+		If Convert.ToInt32(parameters) < 0 Then
+			If grid.PageIndex < grid.PageCount - 1 Then
+				grid.PageIndex += 1
+			Else
+				grid.PageIndex = 0
+			End If
+		Else
+			If grid.PageIndex > 0 Then
+				grid.PageIndex -= 1
+			Else
+				grid.PageIndex = grid.PageCount
+			End If
+		End If
+	End Sub
+End Class
